4c555aef5070c7926d7b2f6e28f4c95220c83ce7
Galca Vlad-Daniel <vladgalca@gmail.com>
2020-11-21 17:36:37 +0200

    Supraincarcare operatori player(>> si <<) + testare afisare in main

--- Tetris++/Tetris++/Player.cpp
+++ Tetris++/Tetris++/Player.cpp
@@ -1,7 +1,19 @@
 #include "Player.h"
 
-Player::Player(std::string name=NULL, uint16_t score=0)
-	:m_name(name),m_score(score)
+Player::Player(std::string name = NULL, uint16_t score = 0)
+	:m_name(name), m_score(score)
 {
 
+}
+
+std::istream& operator>>(std::istream& in, Player& player)
+{
+	in >> player.m_name >> player.m_score;
+	return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Player& player)
+{
+	out << player.m_name << " " << player.m_score << std::endl;
+	return out;
 }
--- Tetris++/Tetris++/Tetris++.cpp
+++ Tetris++/Tetris++/Tetris++.cpp
@@ -1,5 +1,6 @@
 #include "Board.h"
 #include "BlackHole.h"
+#include "Player.h"
 #include <windows.h> // l-am pus pt sleep , il stergem dupa
 
 
@@ -7,17 +8,18 @@ int main()
 {
 	try {
 		Board board(7, 21, 0);
-		board[{1,3}] = 3;
-		std::cout << board;
+		board[{1, 3}] = 3;
+		Player player("Player1", 420);
+		std::cout << player << board;
 		BlackHole hole;
 		hole.eat(board);
 		Sleep(3000);
 		system("CLS");
-		std::cout << board;
+		std::cout << player << board;
 		hole.disappear(board);
 		Sleep(3000);
 		system("CLS");
-		std::cout << board;
+		std::cout << player << board;
 	}
 	catch (const char* errorMessage)
 	{
