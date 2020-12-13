776489045ba80f56117d672e077b559e0aace262
Galca Vlad-Daniel <vladgalca@gmail.com>
2020-12-13 14:11:23 +0200

    Changes: - added UnitTesting to project (doesn't test anything yet -> to be determined what needs testing)

---
+++ Tetris++/TetrisTests/TetrisTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TetrisTests
+{
+	TEST_CLASS(TetrisTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
