8de3f5a8e7a4bea764db8a0dc1a81898a1c20c13
Daniciuc_Razvan <daniciucrazvan@yahoo.com>
2021-01-24 16:21:40 +0200

    Removed classes who were intended for the menu development and organized existing classes using filters

--- Tetris++/Tetris++/Texture.cpp
+++
@@ -1,7 +0,0 @@
-#include "Texture.h"
-
-const std::string Texture::TETRIS_LOGO;
-const std::string Texture::MAIN_MENU_BUTTONS;
-const std::string Texture::MAIN_MENU_BACKGROUND;
-const std::string Texture::GAME_OVER;
-const std::string Texture::VICTORY;
